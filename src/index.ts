9b790d0ad26cf8c8f70c00189ea7aea66b093ff8
Stefanie Doll <stefie.doll@googlemail.com>
2019-09-04 17:12:19 +0200

    Add updated host functions to UI

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,19 @@ interface Contract extends ASUtil {
 }
 
 interface Env extends ImportsObject {
+  console?: {
+    console_log: (value: any) => void
+  }
   env?: {
     memory?: WebAssembly.Memory,
     trace?: (msg: number, numArgs?: number, ...args: any[]) => void,
     ext_get_storage?: (key_ptr: number) => number, 
-    ext_input_copy?: (dest_ptr: number, offset: number, len: number) => void,
-    ext_input_size?: () => number,
-    ext_return?: (data_ptr: number, data_len: number) => void,
+    ext_println?: (ptr: number, len: number) => void,
+    ext_scratch_read?: (dest_ptr: number, offset: number, len: number) => void,
     ext_scratch_size?: () => number,
-    ext_scratch_copy?: (dest_ptr: number, offset: number, len: number) => void,
-    ext_set_storage?: (key_ptr: number, value_non_null: number, value_ptr: number, value_len: number) => void,
-
+    ext_scratch_write?: (src_ptr: number, len: number) => void,
+    ext_set_rent_allowance?: (value_ptr: number, value_len: number) => void,
+    ext_set_storage?: (key_ptr: number, value_non_null: number, value_ptr: number, value_len: number) => void
   }
 }
 
@@ -25,35 +27,47 @@ const memory = new WebAssembly.Memory({
   initial: 2,
   maximum: 16,
 });
-let scratchBuf: any = [];
+let scratchBuf: Uint8Array = new Uint8Array(0);
 
 const env: Env = {
+  console: {
+    console_log: function (value: any) {
+      console.log('console_log', value )
+    }
+  },
   env: {
     memory: memory,
     ext_get_storage: function (key_ptr: number) {
       console.log('ext_get_storage', key_ptr )
       return key_ptr;
     }, 
-    ext_input_copy: function(dest_ptr: number, offset: number, len: number) {
-      console.log(`ext_scratch_read(ptr=${dest_ptr}, offset=${offset}, len="${len})`);
+    ext_println: function (ptr: number, len: number) {
+      console.log('ext_println', ptr, len )
+      // let str = readString(ptr, len);
+      // console.log('LOGGED: ', str);
+    },
+    ext_scratch_read: function(dest_ptr: number, offset: number, len: number) {
+      console.log(`ext_scratch_read(dest_ptr=${dest_ptr}, offset=${offset}, len="${len})`);
       let mem = new Uint8Array(memory.buffer);
-      for (var i = 0; i < len; i++) {
+      for (let i = 0; i < len; i++) {
           mem[dest_ptr + i] = scratchBuf[i];
       }
-    },
-    ext_input_size: function() {
-      console.log('ext_input_size', scratchBuf.length )
-      return scratchBuf.length;
-    },
-    ext_return: function (data_ptr: number, data_len: number) {
-      console.log('ext_return', data_ptr, data_len)
     },
     ext_scratch_size: function () {
       console.log('ext_scratch_size', scratchBuf.length )
       return scratchBuf.length;
     },
-    ext_scratch_copy: function (dest_ptr: number, offset: number, len: number) {
-      console.log('ext_scratch_copy', dest_ptr, offset, len)
+    ext_scratch_write: function (src_ptr: number, len: number) {
+      console.log('ext_scratch_write', src_ptr, len )
+      var mem = new Uint8Array(memory.buffer);
+      console.log('ext_scratch_write mem.values', mem.values)
+      // for (let i = 0; i < len; i++) {
+      //     scratchBuf.push(mem[ptr + i]);
+      // }
+      return scratchBuf.length;
+    },
+    ext_set_rent_allowance: function (value_ptr: number, value_len: number) {
+      console.log('ext_return', value_ptr, value_len)
     },
     ext_set_storage: function (key_ptr: number, value_non_null: number, value_ptr: number, value_len: number) {
       console.log('ext_set_storage', key_ptr, value_non_null, value_ptr, value_len)
